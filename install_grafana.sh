7dddd474f62f799d53ac71b08d39ce9eebec6036
amanpreet9391 <amanpreet9391@gmail.com>
2021-04-01 04:18:16 +0530

    script to install grafana

---
+++ install_grafana.sh
@@ -0,0 +1,15 @@
+cat <<EOF > /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/oss/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+sudo yum install grafana
+systemctl start grafana-server
+systemctl status grafana-server
+#http://Your Server IP or Host Name:3000/
